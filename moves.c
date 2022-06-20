b02e6cddd27c95edbba9cfc09b16d6e73c016b9d
Babara Aura <baura@un-r2.kzn.21-school.ru>
2022-06-21 00:59:46 +0300

    IT WORKS

--- moves.c
+++ moves.c
@@ -6,11 +6,12 @@
 /*   By: baura <baura@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 22:06:41 by baura             #+#    #+#             */
-/*   Updated: 2022/06/21 00:12:22 by baura            ###   ########.fr       */
+/*   Updated: 2022/06/21 00:58:43 by baura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <stdbool.h>
 
 int	close_game(t_game *game)
 {
@@ -35,37 +36,39 @@ void	check_collectibles(t_game *game)
 	if (game->colletctible == 0)
 	{
 		// put_img(game, game->open_exit_img, game->exit_x * game->img_size, game->exit_y * game->img_size);
-		game->exit += 1;
+		game->exit -= 1;
 	}
 }
 
-int	move_check(t_game *game, int x, int y) 
+bool    move_check(t_game *game, int x, int y) 
 {
 	if (game->map[y][x] == '0')
 	{
 		put_img(game, game->player_img, x * game->img_size, y * game->img_size);
 		put_img(game, game->empty_space_img, game->player_x * game->img_size, \
 				game->player_y * game->img_size);
-		return (1);
+		return (true);
 	}
 	if (game->map[y][x] == 'C')
 	{
 		put_img(game, game->player_img, x * game->img_size, y * game->img_size);
 		put_img(game, game->empty_space_img, game->player_x * game->img_size, \
 				game->player_y * game->img_size);
-		game->colletctible--;
+		game->colletctible -= 1;
 		check_collectibles(game);
-		return (1);
+		return (true);
 	}
 	if (game->map[y][x] == 'E') // check
 	{
-		if (game->exit == 1)
-		{
+        if (game->exit == 0)
+        {
+            ft_putstr_fd("You won! Well done!\n", 1);
             mlx_destroy_window(game->mlx, game->mlx_win);
             exit(EXIT_SUCCESS);
         }
+        ft_putstr_fd("You need to collect all coins to open the box\n", 1);
 	}
-	return (0);
+	return (false);
 }
 
 void	move_up(t_game *game)
@@ -142,7 +145,7 @@ void	move_right(t_game *game)
 
 int	key_hook(int keycode, t_game *game)
 {
-	printf("%d\n", keycode);
+	// printf("%d\n", keycode);
 	if (keycode == 13) // W
 		move_up(game);
 	else if (keycode == 0) // A
